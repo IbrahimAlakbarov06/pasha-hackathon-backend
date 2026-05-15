6b91655a92ffd3a3736c5dce747854af4cfdc6c4
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2026-05-15 13:42:51 +0400

    project created

---
+++ src/main/java/com/example/pashahackathonbackend/PashaHackathonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pashahackathonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PashaHackathonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PashaHackathonBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pashahackathonbackend/PashaHackathonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pashahackathonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PashaHackathonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
